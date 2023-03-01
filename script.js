1453a069d593f9ff2523e35c2ce3cfea4ff9f1a7
samxaradzenika <murdoc746@gmail.com>
2023-03-02 01:05:15 +0400

    practice and implemented smooth scroll
    practiced on event propagation, event handlers, events  and so on...

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 ///////////////////////////////////////
 // Modal window
 
@@ -31,11 +29,104 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+
+btnScrollTo.addEventListener(`click`, function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  //scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior:'smooth',
+  // });
+  section1.scrollIntoView({ behavior: `smooth` });
+});
 
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////  PRACTICING ///////////////////////////////////////////
-
-const allSections = document.querySelectorAll(`.section`);
+const header = document.querySelector(`.header`);
+const allSections = document.querySelectorAll(`.section`); //node list
 console.log(allSections);
+document.getElementById(`section--1 `);
+const allButtons = document.getElementsByTagName(`button`); // live list of tags
+console.log(allButtons);
+console.log(document.getElementsByClassName(`btn`)); //also live list
+const message = document.createElement(`div`);
+message.classList.add(`cookie-message`);
+// message.textContent=`we use cookies for improved functionality and analytics.`;
+message.innerHTML = `we use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`;
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    message.remove();
+  });
+message.style.backgroundColor = `#37383d`;
+message.style.width = `120%`;
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + `px`;
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+//Attributes
+
+const logo = document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = `Beautiful minimalist logo`;
+
+//NON standart
+
+console.log(logo.designer);
+console.log(logo.getAttribute(`designer`));
+
+console.log(logo.setAttribute(`company`, `Bankist`));
+
+console.log(logo.getAttribute(`src`));
+
+const link = document.querySelector(`.twitter-link`);
+
+console.log(link.href);
+console.log(link.getAttribute(`href`));
+console.log(logo.dataset.versionNumber);
+
+const h1 = document.querySelector(`h1`);
+const alertH1 = function (e) {
+  alert(`header!!!!`);
+  // h1.removeEventListener(`mouseenter`, alertH1);
+};
+h1.addEventListener(`mouseenter`, alertH1);
+setTimeout(() => h1.removeEventListener(`mouseenter`, alertH1), 3000);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 225)},${randomInt(0, 225)},${randomInt(0, 225)})`;
+
+document.querySelector(`.nav__link`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+  // e.stopImmediatePropagation();
+});
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector(`.nav`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+});
