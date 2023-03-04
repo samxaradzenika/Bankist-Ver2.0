7fcdc33df848d7dcc52a04073537ab6baac5c358
samxaradzenika <murdoc746@gmail.com>
2023-03-04 23:49:00 +0400

    implemented elements revealing on scroll

--- script.js
+++ script.js
@@ -124,6 +124,29 @@ const headerObserver = new IntersectionObserver(stickynav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+//reveal sections
+const allSections = document.querySelectorAll(`.section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove(`section--hidden`);
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add(`section--hidden`);
+});
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////
