1e5fbd7ef9829f1cbec0563f6f048c8c9c00a5b6
samxaradzenika <murdoc746@gmail.com>
2023-03-05 01:58:39 +0400

    implemented sliders

--- script.js
+++ script.js
@@ -145,8 +145,80 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add(`section--hidden`);
+  // section.classList.add(`section--hidden`);
 });
+
+// lazy loading images
+
+const imgTargets = document.querySelectorAll(`img[data-src]`);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  //replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener(`load`, function () {
+    entry.target.classList.remove(`lazy-img`);
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-200px`,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+//slider
+const slides = document.querySelectorAll(`.slide`);
+const btnLeft = document.querySelector(`.slider__btn--left`);
+const btnRight = document.querySelector(`.slider__btn--right`);
+let curSlide = 0;
+const maxSlide = slides.length;
+const dotContainer = document.querySelector(`.dots`);
+
+// const slider = document.querySelector(`.slider`);
+// slider.style.transform = `scale(0.35) translateX(-900px)`;
+// slider.style.overflow = `visible`;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}% )`)
+  );
+};
+
+goToSlide(0);
+
+//next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+btnRight.addEventListener(`click`, nextSlide);
+btnLeft.addEventListener(`click`, prevSlide);
+
+document.addEventListener(`keydown`, function (e) {
+  console.log(e);
+  if (e.key === `ArrowLeft`) prevSlide();
+  e.key === `ArrowRight` && nextSlide();
+});
+
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////
